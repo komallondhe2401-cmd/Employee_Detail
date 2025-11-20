e4ab23b9b4aa58ad5bb48d831594e88cec2654a9
komallondhe <komallondhe2401@gmail.com>
2025-11-20 20:08:28 +0530

    Removed folder from git tracking

--- Numpy/grid.py
+++
@@ -1,5 +0,0 @@
-grid=[[1,2,3],[4,5,6],[7,8,9],[0]]
-
-for rows in grid:
-    for col in rows:
-        print(col)
--- Numpy/hello.py
+++
@@ -1,7 +0,0 @@
-color=input("Enter rose color:")
-Sky_color=input("Enter Sky_color:")
-name=input("Name is:")
-
-print("Roses are",color)
-print("Sky is",Sky_color)
-print("I hate",name)
